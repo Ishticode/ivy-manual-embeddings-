610e2abd03d770ab6ffb9d230e91559b3857378c
Ishtiaq Hussain <attarist@hotmail.com>
2022-02-09 12:51:11 +0000

    time features

--- main_file.py
+++ main_file.py
@@ -13,3 +13,10 @@ df = pd.read_csv("NYCTaxiFares.csv")
 
 #haversine_distance function takes dataframe and its corresponding columns as input
 df["dist_km"] = haversine_distance(df,"pickup_latitude", 'pickup_longitude', 'dropoff_latitude','dropoff_longitude')
+
+#using pickup_datetime column to derive some useful attributes
+df["pickup_datetime"] = pd.to_datetime(df["pickup_datetime"])
+df["EDTdate"] = df['pickup_datetime']- pd.Timedelta(hours=4)
+df['Hour'] = df['EDTdate'].dt.hour
+df["AMorPM"] = np.where(df['Hour']<12,'am','pm')
+df["Weekday"] = df['EDTdate'].dt.strftime("%a")
